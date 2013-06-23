d5b5f388fe6c7b13f933dd3f73a4842562f815d0
William Billingsley <wbillingsley@cantab.net>
2013-06-23 13:47:51 +1000

    Added updateAndFetch, updateSafe, updateUnsafe methods to FindById
    This allows us to remove some boilerplate from the database classes.

--- modules/reactivemongo/src/main/scala/com/impressory/reactivemongo/FindById.scala
+++ modules/reactivemongo/src/main/scala/com/impressory/reactivemongo/FindById.scala
@@ -5,6 +5,7 @@ import com.wbillingsley.handyplay._
 import reactivemongo.api._
 import reactivemongo.bson._
 import play.api.libs.concurrent.Execution.Implicits._
+import reactivemongo.core.commands.GetLastError
 
 trait FindById[T] {
   
@@ -15,19 +16,36 @@ trait FindById[T] {
   implicit val bsonReader:BSONDocumentReader[T];
 
   def byId(id:BSONObjectID) = {    
-    val query = BSONDocument("_id" -> id)
-    val coll = DB.coll(collName)
-    val fo = coll.find(query).one[T]    
-    val rfr = new RefFutureRef(fo.map(Ref(_)))
-    rfr
+    val fo = coll.find(BSONDocument("_id" -> id)).one[T]    
+    new RefFutureOption(fo)
   } 
   
   def manyById(ids:Seq[BSONObjectID]) = {
-    val query = BSONDocument("_id" -> BSONDocument("$in" -> ids))
-    val coll = DB.coll(collName)
-    val fc = coll.find(query).cursor[T]
-    val rfi = new RefEnumIter(fc.enumerateBulks)
-    rfi
+    findMany(BSONDocument("_id" -> BSONDocument("$in" -> ids)))
   }
   
+  def updateAndFetch(query:BSONDocument, update:BSONDocument):Ref[T] = {
+    val c = coll
+    val fle = c.update(query, update, GetLastError(true)) 
+    val fut = fle.map { _ => new RefFutureOption(c.find(query).one[T]) } recover { case x:Throwable => RefFailed(x) }
+    new RefFutureRef(fut)
+  }
+  
+  def updateSafe(query:BSONDocument, update:BSONDocument, item:T):Ref[T] = {
+    val c = coll
+    val fle = c.update(query, update, GetLastError(true)) 
+    val fut = fle.map { _ => RefItself(item) } recover { case x:Throwable => RefFailed(x) }
+    new RefFutureRef(fut)
+  }
+
+  def updateUnsafe(query:BSONDocument, update:BSONDocument, item:T):Ref[T] = {
+    val c = coll
+    val fle = c.update(query, update, GetLastError(false)) 
+    val fut = fle.map { _ => RefItself(item) } recover { case x:Throwable => RefFailed(x) }
+    new RefFutureRef(fut)
+  }
+  
+  def findMany(query:BSONDocument):RefMany[T] = {
+    new RefEnumIter(coll.find(query).cursor[T].enumerateBulks)
+  }
 }

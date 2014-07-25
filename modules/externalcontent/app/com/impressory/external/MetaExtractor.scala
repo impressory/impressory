32aae9f9f7bb855e997867ea93be6441576a974c
William Billingsley <wbillingsley@cantab.net>
2014-07-25 11:56:56 +1000

    MetadataExtractor uses streamed response from WS
    Also fixes a bug where we had been asking for the wrong underlying implementation

--- modules/externalcontent/app/com/impressory/external/MetaExtractor.scala
+++ modules/externalcontent/app/com/impressory/external/MetaExtractor.scala
@@ -5,7 +5,11 @@ import com.wbillingsley.handy._
 import Ref._
 
 import play.api.Play.current
+import play.api.libs.iteratee.{Iteratee, Enumerator, Enumeratee}
+import play.api.libs.ws.WSResponseHeaders
 import play.api.libs.ws.ning.NingWSResponse
+import play.api.mvc.Codec
+
 
 /**
  * Pulls OpenGraph and other data from a web page
@@ -31,83 +35,125 @@ object MetaExtractor {
       case ex: URISyntaxException => Some("(malformed url)")
     }
   }
-    
-  
-  def fetchAndExtract(url:String):Ref[MetaData] = {
-    import play.api.libs.ws.WS
-    import play.api.libs.concurrent.Execution.Implicits._
-  
-    (for {
-      res <- WS.url(url).withHeaders("Accept" -> "text/*").get()
-      title = "<title>([^<]+)</title>".r.findFirstMatchIn(res.body).map(_.group(1))
-    } yield {
-      
-      // Just take the first 10k to avoid parsing very long responses
-      val excerpt = res.underlying[NingWSResponse].ahcResponse.getResponseBodyExcerpt(10000)
-      val html = htmlCleaner.clean(excerpt)
-      
-      val metas = html.getElementsByName("meta", true)
-      
-      def metaName(n:TagNode) = {
-        Option(n.getAttributeByName("name")) orElse Option(n.getAttributeByName("property"))
-      }
-      
-      def findMetaByName(nodes:Seq[TagNode], name:String) = {
-        metas.find { n => 
-          n.hasAttribute("name") && n.getAttributeByName("name") == name
-        } flatMap { n => Option(n.getAttributeByName("content")) }
-      }
-      
-      def findMetaProperty(nodes:Seq[TagNode], property:String) = {
-        metas.find { n => 
-          n.hasAttribute("property") && n.getAttributeByName("property") == property
-        } flatMap { n => Option(n.getAttributeByName("content")) }
-      }
-      
-      def findTagContent(t:TagNode, tagName:String) = {
-        val el = t.getElementsByName(tagName, true).headOption
-        for (e <- el) yield e.getText().toString
-      }      
-      
-      val pairs = for {
-          m <- metas
-          n <- metaName(m)
-          lc = n.toLowerCase()
-          c <- Option(m.getAttributeByName("content"))
-        } yield (lc, c)
-      
-      val metaMap = Map(pairs:_*)
-            
+
+
+  def extractFromHtml(url:String, headers:WSResponseHeaders, excerpt:String):MetaData = {
+    val html = htmlCleaner.clean(excerpt)
+
+    val metas = html.getElementsByName("meta", true)
+
+    def metaName(n:TagNode) = {
+      Option(n.getAttributeByName("name")) orElse Option(n.getAttributeByName("property"))
+    }
+
+    def findMetaByName(nodes:Seq[TagNode], name:String) = {
+      metas.find { n =>
+        n.hasAttribute("name") && n.getAttributeByName("name") == name
+      } flatMap { n => Option(n.getAttributeByName("content")) }
+    }
+
+    def findMetaProperty(nodes:Seq[TagNode], property:String) = {
+      metas.find { n =>
+        n.hasAttribute("property") && n.getAttributeByName("property") == property
+      } flatMap { n => Option(n.getAttributeByName("content")) }
+    }
+
+    def findTagContent(t:TagNode, tagName:String) = {
+      val el = t.getElementsByName(tagName, true).headOption
+      for (e <- el) yield e.getText().toString
+    }
+
+    val pairs = for {
+      m <- metas
+      n <- metaName(m)
+      lc = n.toLowerCase()
+      c <- Option(m.getAttributeByName("content"))
+    } yield (lc, c)
+
+    val metaMap = Map(pairs:_*)
+
+
+    try {
       val m = MetaData(
         title = {
           metaMap.get("og:title") orElse findTagContent(html, "title")
         },
-        
+
         canonicalUrl = {
           metaMap.get("og:url") orElse Some(url)
         },
-          
+
         imageUrl = {
           metaMap.get("og:image")
         },
-        
+
         siteName = {
           metaMap.get("og:site_name") orElse site(url)
         },
-        
+
         summary = {
-          metaMap.get("description") 
+          metaMap.get("description")
         },
-        
+
         noFrame = {
-          res.header("X-Frame-Options").isDefined
+          headers.headers.isDefinedAt("X-Frame-Options")
         }
       )
-      println(m)
       m
-      
-    }).toRef
+    } catch {
+      case x:Throwable => {
+        x.printStackTrace()
+        throw x
+      }
+    }
+  }
+
+  def charSet(contentType:Option[String]):String = contentType match {
+    case Some(ct) => {
+      val arr = ct.split("charset=")
+      if (arr.length == 2) {
+        arr(1)
+      } else "utf-8"
+    }
+    case _ => "utf-8"
+  }
+
+  def extractFromTextlikeStream(url:String, headers:WSResponseHeaders, stream:Enumerator[Array[Byte]]):Ref[MetaData] = {
+
+    import play.api.libs.concurrent.Execution.Implicits._
+
+    val contentType = headers.headers("Content-Type").headOption
+    val textCharSet = charSet(contentType)
+
+    var taken = 0
+    val limited = stream through Enumeratee.takeWhile { arr =>
+      taken += arr.length
+      taken < 10000
+    }
+
+    val codec:Codec = Codec.javaSupported(textCharSet)
+    val stringed = limited through Enumeratee.map { arr =>
+      codec.decode(arr)
+    }
+
+    val futRes = stringed.run(Iteratee.consume[String].apply)
+    for {
+      excerpt <- futRes
+    } yield {
+      extractFromHtml(url, headers, excerpt)
+    }
+  }
+
   
+  def fetchAndExtract(url:String):Ref[MetaData] = {
+    import play.api.libs.ws.WS
+    import play.api.libs.concurrent.Execution.Implicits._
+
+    var taken = 0
+    for {
+      (res, enum) <- WS.url(url).withHeaders("Accept" -> "text/*").getStream().toRef
+      meta <- extractFromTextlikeStream(url, res, enum)
+    } yield meta
   }
 }
 
